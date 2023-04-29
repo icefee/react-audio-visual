17224ca939ca5f3f4b8f750efc6e2efe5c7a8b8c
icefee <icefee@outlook.com>
2023-04-30 02:38:05 +0800

    fix resize unbind bug.

--- src/lib/hook/useResizeObserver.ts
+++ src/lib/hook/useResizeObserver.ts
@@ -9,22 +9,27 @@ function useResizeObserver<T extends HTMLElement = HTMLDivElement>() {
     })
 
     const onResize = () => {
-        const width = ref.current.clientWidth;
-        const height = ref.current.clientHeight;
-        setSize({
-            width,
-            height
-        })
+        if (ref.current) {
+            const width = ref.current.clientWidth;
+            const height = ref.current.clientHeight;
+            setSize({
+                width,
+                height
+            })
+        }
     }
 
     useEffect(() => {
+        
         const resizeObserver = new ResizeObserver(onResize)
         resizeObserver.observe(ref.current)
 
         onResize()
 
         return () => {
-            resizeObserver.unobserve(ref.current)
+            if (ref.current) {
+                resizeObserver.unobserve(ref.current)
+            }
         }
     }, [])
 
